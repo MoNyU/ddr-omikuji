4d69174ccdc7e02d4814a5660b76ed5b33a2ed72
MoNyU <monyudou@gmail.com>
2022-01-01 23:32:43 +0900

    曲一覧を2022/01/01時点に更新した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -90,7 +90,8 @@ const IndexPage: FC = () => {
   return (
     <Layout>
       <StyledDescription>
-        DDRに収録されている曲(2020/12/31現在)からランダムに数曲セレクトします
+        DDRに収録されている曲({"2022/01/01"}
+        現在)からランダムに数曲セレクトします
         <br />
         知らない曲や苦手な譜面でもとりあえずトライ！
       </StyledDescription>
