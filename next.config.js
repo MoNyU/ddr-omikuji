ba00bed3185e641e8a8c523ef65c70360d3bd9a6
MoNyU <monyudou@gmail.com>
2022-12-31 23:37:22 +0900

    vanilla-extract, mui/baseのインストール emotionのアンインストール

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
+// @ts-check
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+
+const withVanillaExtract = createVanillaExtractPlugin();
 const isProd = process.env.NODE_ENV === "production";
 
-module.exports = isProd
-  ? {
-      basePath: "/ddr-omikuji",
-      assetPrefix: "/ddr-omikuji",
-    }
-  : {
-      compress: true,
-    };
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  ...(isProd
+    ? {
+        basePath: "/ddr-omikuji",
+        assetPrefix: "/ddr-omikuji",
+      }
+    : {}),
+};
+
+module.exports = withVanillaExtract(config);
